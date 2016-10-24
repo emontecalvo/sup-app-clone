69aa87e58d70814a34632303a7d739497bb52c0e
Paul Chong <paulhyunchong@gmail.com>
2016-10-24 19:20:54 +0000

    add basic authentication with Passport

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
 
 var app = express();
 
@@ -9,9 +12,14 @@ var jsonParser = bodyParser.json();
 var Message = require('./models/message');
 var User = require('./models/user');
 
+var state = {
+    loggedIn: false,
+    userId: ""
+};
+
 // Add your API endpoints here
 var runServer = function(callback) {
-    var databaseUri = process.env.DATABASE_URI || global.databaseUri || 'mongodb://localhost/sup';
+    var databaseUri = process.env.DATABASE_URI || global.databaseUri || 'mongodb://localhost/auth';
     mongoose.connect(databaseUri).then(function() {
         var port = process.env.PORT || 8080;
         var server = app.listen(port, function() {
@@ -23,20 +31,72 @@ var runServer = function(callback) {
     });
 };
 
-if (require.main === module) {
-    runServer();
-};
+var strategy = new BasicStrategy(function(username, password, callback) {
+    User.findOne({
+        username: username
+    }, function (err, user) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        if (!user) {
+            return callback(null, false, {
+                message: 'Incorrect username.'
+            });
+        }
+        state.userId = user._id.toString();
+        user.validatePassword(password, function(err, isValid) {
+            if (err) {
+                return callback(err);
+            }
+
+            if (!isValid) {
+                return callback(null, false, {
+                    message: 'Incorrect password.'
+                });
+            }
+            return callback(null, user);
+        });
+    });
+});
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+
+
+app.get('/hidden', passport.authenticate('basic', {session: false}), function(req, res) {
+    state.loggedIn = true;
+    res.json({
+        message: 'Authenticated...'
+    });
+});
+
+
+
+// app.post('/message', jsonParser, function(req, res) {
+//     Message.create({from: "bob", to: "sue", text: "hello"}, function(err, message){
+//         if (err) {
+//             res.json(err);
+//         }
+        
+//         res.json(message);
+//     })
+    
+// })
 
 //Messages API
 app.get('/messages', jsonParser,function(req, res) {
-    var query = {} ;
+    var query = {};
     if (req.query.to !== undefined) {
         query.to = req.query.to;
     }
     if (req.query.from !== undefined) {
         query.from = req.query.from;
     }
-
+    
+    
     Message.find(query).populate('from to').exec(function(err, messages) {
         if(err) {
             return res.status(500).json({
@@ -49,47 +109,53 @@ app.get('/messages', jsonParser,function(req, res) {
 });
 
 app.post('/messages', jsonParser, function(req, res) {
-    if (req.body.text === undefined) {
-        return res.status(422).json({
-            message: "Missing field: text"
-        });
-    } else if (typeof req.body.text !== 'string') {
-        return res.status(422).json({
-            message: "Incorrect field type: text"
-        });
-    } else if (typeof req.body.to !== 'string') {
-        return res.status(422).json({
-           message: "Incorrect field type: to"
-        });
-    } else if (typeof req.body.from !== 'string') {
-        return res.status(422).json({
-            message: "Incorrect field type: from"
-        });
-    }
+    if (state && req.body.from === state.userId) {
+        if (req.body.text === undefined) {
+            return res.status(422).json({
+                message: "Missing field: text"
+            });
+        } else if (typeof req.body.text !== 'string') {
+            return res.status(422).json({
+                message: "Incorrect field type: text"
+            });
+        } else if (typeof req.body.to !== 'string') {
+            return res.status(422).json({
+               message: "Incorrect field type: to"
+            });
+        } else if (typeof state.userId !== 'string') {
+            return res.status(422).json({
+                message: "Incorrect field type: from"
+            });
+        }
     
-    User.find({_id: req.body.from}, function(err, user){
-        User.find({_id: req.body.to}, function(err, user2){
-            if (user.length === 0) {
-                return res.status(422).json({
-                    message: "Incorrect field value: from"
+        User.find({_id: state.userId}, function(err, user){
+            User.find({_id: req.body.to}, function(err, user2){
+                if (user.length === 0 && user) {
+                    return res.status(422).json({
+                        message: "Incorrect field value: from"
+                    });
+                }
+                if (user2.length === 0 && user2) {
+                    return res.status(422).json({
+                        message: "Incorrect field value: to"
+                    });
+                }
+                Message.create({text: req.body.text, from: req.body.from, to: req.body.to}, function(err, message) {
+                    if (err) {
+                        return res.status(500).json({
+                        message: "Server Error"
+                    });
+                } 
+                res.location('/messages/' + message._id);
+                res.status(201).json({});
                 });
-            }
-            if (user2.length === 0) {
-                return res.status(422).json({
-                    message: "Incorrect field value: to"
-                });
-            }
-            Message.create({text: req.body.text, from: req.body.from, to: req.body.to}, function(err, message) {
-                if (err) {
-                    return res.status(500).json({
-                    message: "Server Error"
-                });
-            } 
-            res.location('/messages/' + message._id);
-            res.status(201).json({});
             });
         });
-    });
+    } else {
+        return res.status(403).json({
+            message: "Forbidden: You can not send a message as someone else!"
+        });
+    }
 });
 
 app.get('/messages/:messageId', function(req, res) {
@@ -125,7 +191,7 @@ app.get('/users/:userId', function(req, res) {
            return res.status(500).json({
                message: 'Server Error'
            });
-       } else if (user === null) {
+       } else if (!user) {
            return res.status(404).json({
                message: 'User not found'
            });
@@ -140,27 +206,103 @@ app.get('/users/:userId', function(req, res) {
 });
 
 app.post('/users', jsonParser, function (req, res) {
-    if(Object.keys(req.body).length === 0) {
-        res.status(422).json({
+    if(!req.body) {
+        return res.status(422).json({
             message: "Missing field: username"
         });
-    } else if ((typeof req.body.username) === 'string'){
-        User.create({
-            username: req.body.username 
-        }, function(err, user) {
-            if(err) {
-                return res.status(500).json({
-                    message: 'Server Error'
-                });
-            }
-            res.location('/users/' + user._id);
-            res.status(201).json({});
-        });
-    } else {
-        res.status(422).json({
-            message: "Incorrect field type: username"
+    }
+    
+    if (!('username' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: username'
         });
     }
+
+    var username = req.body.username;
+
+    if (typeof username !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: username'
+        });
+    }
+
+    username = username.trim();
+
+    if (username === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: username'
+        });
+    }
+
+    if (!('password' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: password'
+        });
+    }
+
+    var password = req.body.password;
+
+    if (typeof password !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: password'
+        });
+    }
+
+    password = password.trim();
+
+    if (password === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: password'
+        });
+    }
+
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+
+        bcrypt.hash(password, salt, function(err, hash) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Internal server error'
+                });
+            }
+
+            var user = new User({
+                username: username,
+                password: hash
+            });
+
+            user.save(function(err) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Internal server error'
+                    });
+                }
+
+                return res.status(201).json({});
+            });
+        });
+    });
+    // if ((typeof req.body.username) === 'string'){
+    //     User.create({
+    //         username: req.body.username 
+    //     }, function(err, user) {
+    //         if(err) {
+    //             return res.status(500).json({
+    //                 message: 'Server Error'
+    //             });
+    //         }
+    //         res.location('/users/' + user._id);
+    //         res.status(201).json({});
+    //     });
+    // } else {
+    //     res.status(422).json({
+    //         message: "Incorrect field type: username"
+    //     });
+    // }
 });
 
 app.put('/users/:userId', jsonParser, function(req, res) {
@@ -192,7 +334,7 @@ app.delete('/users/:userId', jsonParser, function(req, res) {
            return res.status(500).json({
                message: 'Server Error'
            });
-       } else if (user === null) {
+       } else if (!user) {
            return res.status(404).json({
                message: 'User not found'
            });
@@ -202,6 +344,10 @@ app.delete('/users/:userId', jsonParser, function(req, res) {
     });
 });
 
+if (require.main === module) {
+    runServer();
+};
 exports.app = app;
 exports.runServer = runServer;
+
 
